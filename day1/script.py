9bca09a843771078fbd9715efd1ab7707cfeb12d
Jamie Hong <jamiehong@berkeley.edu>
2021-12-22 20:55:04 -0800

    [1][2] python

--- day1/script.py
+++ day1/script.py
@@ -11,7 +11,7 @@ with open(path, newline='') as f:
     input_data = [int(line[0]) for line in reader]
     data = input_data
 
-# Solution
+# Solution: part 1
 def count_increase(data):
     """Return the number of times an element of DATA increases from its
     previous element.
@@ -21,7 +21,22 @@ def count_increase(data):
     """
     return sum([data[i] > data[i-1] for i in range(1, len(data))])
 
-output = str(count_increase(data))
+# output = str(count_increase(data))
+
+def count_increase_window(data, window):
+    """Return the number of times a WINDOW-sized window of elements of DATA
+    increases from the previous window.
+    >>> data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    >>> count_increase_window(data, 3)
+    5
+    """
+    def wsum(i):
+        """The window sum."""
+        return sum([data[i + k] for k in range(window)])
+    return sum([wsum(i) < wsum(i+1) for i in range(len(data) - window)])
+
+# Solution: part 2
+output = str(count_increase_window(data, 3))
 
 # Write the output
 path = 'output.txt'

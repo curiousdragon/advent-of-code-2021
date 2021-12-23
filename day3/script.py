470144b0edfd9d2f53cb36a945a038561207de02
Jamie Hong <jamiehong@berkeley.edu>
2021-12-23 15:40:55 -0800

    [3][2] python

--- day3/script.py
+++ day3/script.py
@@ -11,7 +11,7 @@ with open(path, newline='') as f:
     input_data = [line[0] for line in reader]
     data = input_data
 
-# Solution
+# Solution: part 1
 def diagnostics(data):
     """
     Calculate the gamma rate and the epsilon rate.
@@ -32,8 +32,52 @@ def diagnostics(data):
     rates = [int(rates[0], base=2), int(rates[1], base=2)]
     return rates
 
+# Solution: part 2
+def diagnostics2(data):
+    """
+    Calculate the oxygen generator rating and the CO2 scrubber rating.
+    >>> data = ["00100", "11110", "10110", "10111", "10101", "01111",
+    ...     "00111", "11100", "10000", "11001", "00010", "01010"]
+    >>> diagnostics2(data)
+    [23, 10]
+    """
+    switches = [1, -1]
+    subset = {1: data[:], -1: data[:]}
+    for i in range(len(data[0])):
+        # Check if we're done
+        if len(subset[1]) == 1:
+            if 1 in switches:
+                switches.remove(1)
+        if len(subset[-1]) == 1:
+            if -1 in switches:
+                switches.remove(-1)
+        # Initialize counts
+        count = {1: {"0": 0, "1": 0}, 
+                -1: {"0": 0, "1": 0}}
+        # switch = 1 = Oxygen: most common bit is max
+        # switch = -1 = CO2: least common bit is max
+        for switch in switches:
+            # Compile counts for this bit
+            # print(switch)
+            for bnum in subset[switch]:
+                count[switch][bnum[i]] += 1
+            if count[switch]["0"] == count[switch]["1"]:
+                # print(f"equal, choose:", max(switch, 0))
+                subset[switch] = [n for n in subset[switch]
+                        if n[i] == str(max(switch, 0))]
+            elif count[switch]["0"] > count[switch]["1"]:
+                # print(f"0 greater, choose:", max(0, 0 - switch))
+                subset[switch] = [n for n in subset[switch]
+                        if n[i] == str(max(0, 0 - switch))]
+            else:
+                # print(f"1 greater, choose:", min(1, switch + 1))
+                subset[switch] = [n for n in subset[switch]
+                        if n[i] == str(min(1, switch + 1))]
+        # print(subset)
+    return [int(subset[1][0], base=2), int(subset[-1][0], base=2)]
 
-rates = diagnostics(data)
+# rates = diagnostics(data)
+rates = diagnostics2(data)
 output = str(rates) + '\n' + str(rates[0] * rates[1])
 
 # Write the output
